cab61e3ca381cc4f8d8fb0860f6d438937e7c7cb
Eden Xu <130119691+FlyingPufferFish@users.noreply.github.com>
2024-03-28 05:23:24 +0000

    Test hashing for Coord, Colour, Tups, Tup of Tups

---
+++ search/pq-test.py
@@ -0,0 +1,39 @@
+from core import PlayerColor, Coord, PlaceAction
+from collections import defaultdict as dd
+from queue import PriorityQueue as pq
+
+q = pq()
+q.put(["hi"])
+q.put(["bye"])
+
+# print(q.get())
+# print(q.get())
+# print(q.empty())
+
+print("Coord Hash")
+c1 = Coord(5, 2)
+c2 = Coord(5, 2)
+c3 = Coord(2, 5)
+print(hash(c1))
+print(hash(c2))
+print(hash(c1) == hash(c2))
+# True: same coord hashes to same value
+
+print(hash(c1) == hash(c3))
+# False: diff coord hashes to diff value
+
+print("\nPlayer Colour Hash")
+print(hash(PlayerColor.RED))
+print(hash(PlayerColor.BLUE))
+# player colours hash to diff values
+
+print("\n(Coord, Colour) Hash")
+d1 = (c1, PlayerColor.RED), 
+d2 = (c2, PlayerColor.BLUE)
+
+print(hash(d1))
+print(hash(d2))
+
+print("\nTuple of (Coord, Colour) Hash")
+e1 = (d1, d2)
+print(hash(e1))
